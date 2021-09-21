5eb5b6c84ae074482066c26b84d52b23f3db7c03
Trace <tracedelange@me.com>
2021-09-21 13:15:17 -0500

    commit tree abac16ead484189e5759cf307dc2820ca1bef9d4

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,13 +2,29 @@ class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   def index
+    session[:page_views] ||= 0
+    
+    
+    
+    
+    
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
-
+  
   def show
-    article = Article.find(params[:id])
-    render json: article
+    
+    session[:page_views] ||= 0
+    session[:page_views] = Integer(session[:page_views]) + 1 
+
+    if session[:page_views] >= 3
+      render json: { error: "Maximum pageview limit reached"  }, status: 401
+
+
+    else
+      article = Article.find(params[:id])
+      render json: article
+    end
   end
 
   private
